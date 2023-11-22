07d21bcac7164607387a9360abede058af3f59d1
Nour Adawi <nouradwi12@gmail.com>
2023-11-22 12:43:34 +0000

    feat: profile place holder

--- src/pages/Chat/Profile.tsx
+++ src/pages/Chat/Profile.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Box, Typography } from "@mui/material";
 
+import user from "../../assets/user.png";
 interface Props {
   avatar?: string;
   name?: string;
@@ -8,9 +9,21 @@ interface Props {
 
 export const Profile: FC<Props> = ({ avatar, name }) => {
   return (
-    <Box>
-      <Typography>{name}</Typography>
-      <img src={avatar} />
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      mt={3}
+    >
+      <Typography variant="h3">{name ?? "Coming soon!"}</Typography>
+      {avatar ? (
+        <img src={avatar} />
+      ) : (
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <img alt="logo" src={user} />
+        </Box>
+      )}
     </Box>
   );
 };
